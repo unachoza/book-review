778c0773c45e4a8bfe12ef4c0b528b05ff0089ee
arianna choza <unachoza@gmail.com>
2025-01-06 23:22:08 -0500

    delete book

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { ChangeEvent, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import "./App.css";
 
+type Book = {
+	id: string;
+	name: string;
+};
+
 function App() {
-	const [count, setCount] = useState(0);
 	const [input, setInput] = useState("");
-	const [books, setBooks] = useState<string[]>([]);
+	const [books, setBooks] = useState<Book[]>([]);
 
 	//crud
 	//keep track of books
@@ -12,30 +17,42 @@ function App() {
 	//remove book
 	//edit book
 
-	const addBook = () => {
-		const updatedBooks = [...books, input];
-		setBooks(updatedBooks);
-	};
 	const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
 		const { value, name } = e.target;
 		let newValue = value;
 		setInput(newValue);
 	};
 
+	const addBook = () => {
+		const updatedBooks = [...books, { id: uuidv4(), name: input }];
+		setBooks(updatedBooks);
+		setInput("");
+	};
+
+  const deleteBook = (id:string):any => {
+    const filteredBooks = books.filter(book => book.id !== id)
+    setBooks(filteredBooks)
+  }
+
 	return (
 		<>
 			<h1>Books</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				<button>count is {books.length}</button>
 			</div>
 			<div className="card">
-				<input type="text" onChange={(e) => handleInput(e)} />
+				<input type="text" value={input} onChange={(e) => handleInput(e)} />
 				<button onClick={addBook}>Add Book</button>
 			</div>
 			<div>
 				<h2>these books</h2>
 				{books.map((book) => {
-					return <div>{book}</div>;
+					return (
+						<div className="single-book" key={book.id}>
+							<div>{book.name}</div>
+							<button onClick={() => deleteBook(book.id)}>remove</button>
+						</div>
+					);
 				})}
 			</div>
 		</>
